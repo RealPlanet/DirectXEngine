955964e10924cd1a32d4fa040150a0f6780cf939
Fabio R <fabioruscica@hotmail.it>
2020-12-25 17:44:31 +0100

    Added foundation of a graphics engine

---
+++ DirectXGame/Engine/GraphicsEngine.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <d3d11.h>
+
+class GraphicsEngine
+{
+public:
+	// Initialize GraphicsEngine and DirectX 11 Device
+	bool init();
+	// Release all the occupied resources
+	bool release();
+	static GraphicsEngine* get();
+
+private:
+	ID3D11Device* m_d3d_device;
+	D3D_FEATURE_LEVEL m_feature_level;
+	ID3D11DeviceContext* m_imm_context;
+};
+
